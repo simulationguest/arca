c2678099364e490138d54305570b73c036b8d75c
Leopold Jofer <projekte@leoj.de>
2025-01-11 17:47:08 +0100

    add extraction

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,35 @@
-use std::path::PathBuf;
+use crate::sqlar::extract_archive;
+use crate::Cmd::Extract;
 use clap::{Parser, Subcommand};
 use sqlar::create_archive;
+use std::path::{Path, PathBuf};
+
 mod sqlar;
 
 #[derive(Parser, Debug)]
 #[command(version, about)]
 struct Args {
     #[command(subcommand)]
-    cmd: Cmd
+    cmd: Cmd,
 }
 
 #[derive(Debug, Subcommand)]
 enum Cmd {
-    Create{path: String}
+    Create { path: PathBuf },
+    Extract { from: PathBuf, to: PathBuf },
 }
 
 fn main() {
     let args = Args::parse();
 
     match args.cmd {
-        Cmd::Create{path} => {
-            create_archive(&path, &format!("{path}.sqlar")).unwrap();
+        Cmd::Create { path } => {
+            let mut out_path = path.as_os_str().to_os_string();
+            out_path.push(".sqlite");
+            create_archive(&path, Path::new(&out_path)).unwrap();
+        }
+        Extract { from, to } => {
+            extract_archive(&from, &to).unwrap();
         }
     }
-
 }
--- src/sqlar.rs
+++ src/sqlar.rs
@@ -1,8 +1,12 @@
+use flate2::read::{DeflateDecoder, DeflateEncoder};
+use flate2::Compression;
 use rusqlite::Connection;
-use std::fs::File;
+use std::fs::{File, Permissions};
 use std::io;
+use std::io::Write;
 use std::os::unix::fs::{MetadataExt, PermissionsExt};
-use std::time::SystemTime;
+use std::path::Path;
+use std::time::{Duration, SystemTime};
 use walkdir::WalkDir;
 
 #[derive(Debug, thiserror::Error)]
@@ -23,7 +27,7 @@ pub enum Error {
     FailedToConvertString,
 }
 
-pub fn create_archive(dir: &str, dest: &str) -> Result<(), Error> {
+pub fn create_archive(dir: &Path, dest: &Path) -> Result<(), Error> {
     if std::fs::exists(dest)? {
         return Err(Error::FileAlreadyExists);
     }
@@ -32,13 +36,16 @@ pub fn create_archive(dir: &str, dest: &str) -> Result<(), Error> {
 
     let tx = conn.transaction()?;
 
-    tx.execute("CREATE TABLE sqlar(
+    tx.execute(
+        "CREATE TABLE sqlar(
       name TEXT PRIMARY KEY,
       mode INT,
       mtime INT,
       sz INT,
       data BLOB
-    );", ())?;
+    );",
+        (),
+    )?;
 
     let mut stmt = tx.prepare(
         "INSERT INTO sqlar (name, mode, mtime, sz, data) VALUES (?1, ?2, ?3, ?4, ZEROBLOB(?5))",
@@ -46,7 +53,11 @@ pub fn create_archive(dir: &str, dest: &str) -> Result<(), Error> {
 
     let files = WalkDir::new(dir).into_iter();
 
+    let mut buf: Vec<u8> = Vec::new();
+
     for entry in files {
+        buf.clear();
+
         let entry = entry?;
 
         if !entry.file_type().is_file() {
@@ -71,16 +82,20 @@ pub fn create_archive(dir: &str, dest: &str) -> Result<(), Error> {
 
         let size = metadata.size();
 
-        let _ = stmt.execute((name, permissions, last_modified, size, size))?;
+        let data = File::open(entry.path())?;
+
+        let mut compress = DeflateEncoder::new(data, Compression::best());
+
+        std::io::copy(&mut compress, &mut buf)?;
+
+        let _ = stmt.execute((name, permissions, last_modified, size, buf.len()))?;
 
         let row_id = tx.last_insert_rowid();
 
         let mut blob =
             tx.blob_open(rusqlite::DatabaseName::Main, "sqlar", "data", row_id, false)?;
 
-        let mut data = File::open(entry.path())?;
-
-        std::io::copy(&mut data, &mut blob)?;
+        blob.write_all(&buf)?;
     }
 
     drop(stmt);
@@ -90,4 +105,35 @@ pub fn create_archive(dir: &str, dest: &str) -> Result<(), Error> {
     Ok(())
 }
 
+pub fn extract_archive(from: &Path, to: &Path) -> Result<(), Error> {
+    let conn = Connection::open(from)?;
 
+    let mut stmt = conn.prepare("SELECT name, mode, mtime, rowid FROM sqlar;")?;
+    let mut rows = stmt.query([])?;
+
+    while let Some(row) = rows.next()? {
+        let name: String = row.get(0)?;
+        let mode: u32 = row.get(1)?;
+        let mtime: u64 = row.get(2)?;
+        let row_id: i64 = row.get(3)?;
+
+        let blob = conn.blob_open(rusqlite::DatabaseName::Main, "sqlar", "data", row_id, true)?;
+
+        let mut decoder = DeflateDecoder::new(blob);
+
+        let name = to.join(Path::new(name.as_str()));
+
+        let mut dir = name.clone();
+        dir.pop();
+        std::fs::create_dir_all(&dir)?;
+
+        let mut file = File::create_new(name)?;
+
+        file.set_modified(SystemTime::UNIX_EPOCH + Duration::from_secs(mtime))?;
+        file.set_permissions(Permissions::from_mode(mode))?;
+
+        io::copy(&mut decoder, &mut file)?;
+    }
+
+    Ok(())
+}
